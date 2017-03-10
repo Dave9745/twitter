23fb5b4ba34faa068b810fe1950274c70d5d2cf6
Dave <dtirnan@hotmail.com>
2017-03-10 17:04:24 +0100

    securité et users

--- twitter/app/AppKernel.php
+++ twitter/app/AppKernel.php
@@ -27,6 +27,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new FOS\UserBundle\FOSUserBundle();
         }
 
         return $bundles;
@@ -51,4 +52,6 @@ class AppKernel extends Kernel
     {
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
+
+
 }
